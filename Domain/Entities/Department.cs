8ced1de1b94d927ffa8c317353a79b6c19013025
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-10-02 19:26:00 +0300

    feat: add Department entity with relations to Mentor and InternshipApplication

---
+++ Domain/Entities/Department.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Department
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } = null!;
+
+        // Iliskiler
+        public ICollection<Mentor>? Mentors { get; set; }
+        public ICollection<InternshipApplication>? Applications { get; set; }
+    }
+}
