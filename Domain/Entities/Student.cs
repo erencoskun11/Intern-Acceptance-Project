7e7bcac9b96aa70212e84b370df28a17f52f1f6d
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-10-02 19:33:57 +0300

    created

---
+++ Domain/Entities/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string FullName { get; set; } = null!;
+
+        [MaxLength(50)]
+        public string? StudentNumber { get; set; } // ogrenci no (opsiyonel ama uniq olabilir)
+
+        [Required]
+        public int ClassYear { get; set; } // sinif (3,4 vb.)
+
+        // Universite ve advisor
+        public int UniversityId { get; set; }
+        public University? University { get; set; }
+
+        public ICollection<InternshipApplication>? Applications { get; set; }
+    }
+}
