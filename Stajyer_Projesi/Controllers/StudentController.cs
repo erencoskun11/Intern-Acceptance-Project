1102b5213a290c0e44ecc2a1b9489bdbc07d09d1
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-10-04 19:45:59 +0300

    feat(controller) :created

---
+++ Stajyer_Projesi/Controllers/StudentController.cs
@@ -0,0 +1,46 @@
+﻿using Application.DTOs.Student;
+using Application.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly IStudentService _service;
+
+        public StudentController(IStudentService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<StudentListDto>>> GetAll() => Ok(await _service.GetAllAsync());
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<StudentListDto>> GetById(int id)
+        {
+            var result = await _service.GetByIdAsync(id);
+            if (result == null) return NotFound();
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<bool>> Create(StudentCreateDto dto) => Ok(await _service.CreateAsync(dto) > 0);
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<bool>> Update(int id, StudentUpdateDto dto)
+        {
+            await _service.UpdateAsync(id, dto);
+            return true;
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<bool>> Delete(int id)
+        {
+            await _service.DeleteAsync(id);
+            return true;
+        }
+    }
+}
