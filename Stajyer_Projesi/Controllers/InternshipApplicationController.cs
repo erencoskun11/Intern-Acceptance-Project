42ff32c77d1bc1bc5321f6221b5081df246226fd
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-10-04 19:42:08 +0300

    feat(controller) created

---
+++ Stajyer_Projesi/Controllers/InternshipApplicationController.cs
@@ -0,0 +1,46 @@
+﻿using Application.DTOs.InternshipApplication;
+using Application.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InternshipApplicationController : ControllerBase
+    {
+        private readonly IInternshipApplicationService _service;
+
+        public InternshipApplicationController(IInternshipApplicationService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<InternshipApplicationListDto>>> GetAll() => Ok(await _service.GetAllAsync());
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<InternshipApplicationListDto>> GetById(int id)
+        {
+            var result = await _service.GetByIdAsync(id);
+            if (result == null) return NotFound();
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<bool>> Create(InternshipApplicationCreateDto dto) => Ok(await _service.CreateAsync(dto) > 0);
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<bool>> Update(int id, InternshipApplicationUpdateDto dto)
+        {
+            await _service.UpdateAsync(id, dto);
+            return true;
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<bool>> Delete(int id)
+        {
+            await _service.DeleteAsync(id);
+            return true;
+        }
+    }
+}
