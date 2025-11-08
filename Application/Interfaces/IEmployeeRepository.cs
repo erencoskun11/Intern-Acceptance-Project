3434bf8057f5ef9b191b5aeac149878eba3bee26
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-11-08 19:47:37 +0300

    feat(IRepoitory) : created

---
+++ Application/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,12 @@
+﻿// IEmployeeRepository.cs
+using Domain.Entities;
+
+namespace Application.Interfaces
+{
+    public interface IEmployeeRepository : IGenericRepository<Employee>
+    {
+        Task<Employee?> GetByEmailAsync(string email);
+        Task<IEnumerable<Employee>> GetByDepartmentIdAsync(int departmentId);
+    }
+}
+
