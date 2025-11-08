5867f74f3521f44ad4c3b38ef8cfed5e655a0dc7
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-11-08 19:41:56 +0300

    feat(dto): created AssignmentCreateDto

---
+++ Application/DTOs/Assignment/AssignmentCreateDto.cs
@@ -0,0 +1,13 @@
+﻿// AssignmentCreateDto.cs
+namespace Application.DTOs.Assignment
+{
+    public class AssignmentCreateDto
+    {
+        public int InventoryItemId { get; set; }
+        public int? EmployeeId { get; set; }
+        public int? StudentId { get; set; }
+        public DateTime AssignedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? ExpectedReturnAt { get; set; }
+        public string? Notes { get; set; }
+    }
+}
