29f8cdd1020958a9a407cc41b15be2941bf020f2
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-11-08 19:44:55 +0300

    feat(IRepository) :  created

---
+++ Application/Interfaces/IAssignmentRepository.cs
@@ -0,0 +1,11 @@
+﻿// IAssignmentRepository.cs
+using Domain.Entities;
+
+namespace Application.Interfaces
+{
+    public interface IAssignmentRepository : IGenericRepository<Assignment>
+    {
+        Task<IEnumerable<Assignment>> GetByEmployeeIdAsync(int employeeId);
+        Task<IEnumerable<Assignment>> GetByStudentIdAsync(int studentId);
+    }
+}
