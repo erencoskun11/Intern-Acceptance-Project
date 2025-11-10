7ae83b65659bc31d1253059a091d90e3beacd929
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-11-10 18:13:57 +0300

    feat():  created

---
+++ Application/Interfaces/IMaintenanceRepository.cs
@@ -0,0 +1,10 @@
+﻿// IMaintenanceRepository.cs
+using Domain.Entities;
+
+namespace Application.Interfaces
+{
+    public interface IMaintenanceRepository : IGenericRepository<Maintenance>
+    {
+        Task<IEnumerable<Maintenance>> GetByInventoryItemIdAsync(int inventoryItemId);
+    }
+}
