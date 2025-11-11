7cf3e3c8279150e271c2ed9c09ef186f39c24673
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-11-11 17:38:19 +0300

    created

---
+++ Application/DTOs/InternshipApplication/InternshipApplicationCreateDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Domain.Enums;
+
+namespace Application.DTOs.InternshipApplication
+{
+    public class InternshipApplicationCreateDto
+    {
+        public int StudentId { get; set; }
+        public int DepartmentId { get; set; }
+        public int? EmployeeId { get; set; }
+        public InternshipRole Role { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool IsMandatory { get; set; } = false;
+        public bool IsVolunteer { get; set; } = false;
+    }
+}
