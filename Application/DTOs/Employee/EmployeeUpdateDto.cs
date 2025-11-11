69b37beba96a292d6f21b6124b00cff6858ad47b
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-11-11 17:36:48 +0300

    feat () : created

---
+++ Application/DTOs/Employee/EmployeeUpdateDto.cs
@@ -0,0 +1,15 @@
+﻿// EmployeeUpdateDto.cs
+namespace Application.DTOs.Employee
+{
+    public class EmployeeUpdateDto
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? Role { get; set; }
+        public int? DepartmentId { get; set; }
+        public DateTime? EndDate { get; set; }
+    }
+}
