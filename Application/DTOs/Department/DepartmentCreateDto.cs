99b1a684e51c5868717cf4332921b23c61d6f728
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-11-11 17:35:01 +0300

    feat(dto) : created

---
+++ Application/DTOs/Department/DepartmentCreateDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.DTOs.Department
+{
+    public class DepartmentCreateDto
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } = null!;
+    }
+}
