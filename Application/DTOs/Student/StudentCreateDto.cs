7f6074aad45c4ffb2a7ea53196aa76e74ce3b276
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-11-11 17:40:50 +0300

    created

---
+++ Application/DTOs/Student/StudentCreateDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.DTOs.Student
+{
+    public class StudentCreateDto
+    {
+        
+        public string FullName { get; set; } = null!;
+
+        public string? StudentNumber { get; set; }
+        public int ClassYear { get; set; }
+        public int UniversityId { get; set; }
+    }
+}
