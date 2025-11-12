39edae12f229b129458474ea71e42e7764f061d5
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-11-12 19:47:43 +0300

    created

---
+++ Application/DTOs/InternshipApplication/InternshipApplicationListDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Domain.Enums;
+
+namespace Application.DTOs.InternshipApplication
+{
+    public class InternshipApplicationListDto
+    {
+        public int Id { get; set; }
+        public string StudentName { get; set; } = null!;
+        public string DepartmentName { get; set; } = null!;
+        public string? EmployeeName { get; set; } // Bu alan eklendi
+        public string Role { get; set; } = null!;
+        public decimal TaskSuccessPercent { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool IsMandatory { get; set; }
+        public bool IsVolunteer { get; set; }
+    }
+}
