23172a873b957087a679413eafbb739945034e46
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-11-13 16:13:30 +0300

    created

---
+++ Application/DTOs/Department/DepartmentUpdateDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.DTOs.Department
+{
+    public class DepartmentUpdateDto
+    {
+     public int Id { get; set; }
+
+     
+     public string Name { get; set; } = null!;
+    }
+}
