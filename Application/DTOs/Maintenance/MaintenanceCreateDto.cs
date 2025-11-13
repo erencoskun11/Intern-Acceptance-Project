a6f91ff2538a8f6d7c153977c3f0e3178a7843cd
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-11-13 16:13:38 +0300

    created

---
+++ Application/DTOs/Maintenance/MaintenanceCreateDto.cs
@@ -0,0 +1,13 @@
+﻿
+// MaintenanceCreateDto.cs
+namespace Application.DTOs.Maintenance
+{
+    public class MaintenanceCreateDto
+    {
+        public int InventoryItemId { get; set; }
+        public DateTime ReportedAt { get; set; } = DateTime.UtcNow;
+        public string? Description { get; set; }
+        public string? PerformedBy { get; set; }
+        public decimal? Cost { get; set; }
+    }
+}
