94ef48720a8153cf40f0eb6f624883af8044b043
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-11-15 17:30:06 +0300

    created

---
+++ Application/DTOs/InventoryItem/InventoryItemCreateDto.cs
@@ -0,0 +1,13 @@
+﻿// InventoryItemCreateDto.cs
+namespace Application.DTOs.InventoryItem
+{
+    public class InventoryItemCreateDto
+    {
+        public string ItemCode { get; set; } = null!;
+        public string Category { get; set; } = null!;
+        public string? Brand { get; set; }
+        public string? Model { get; set; }
+        public string? SerialNumber { get; set; }
+        public string? Location { get; set; }
+    }
+}
