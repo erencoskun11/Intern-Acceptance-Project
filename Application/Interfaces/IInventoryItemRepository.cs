38901741d1e02c6412d87ee95f29741a296a6be7
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-11-15 17:33:32 +0300

    feat(repository) : created

---
+++ Application/Interfaces/IInventoryItemRepository.cs
@@ -0,0 +1,10 @@
+﻿// IInventoryItemRepository.cs
+using Domain.Entities;
+
+namespace Application.Interfaces
+{
+    public interface IInventoryItemRepository : IGenericRepository<InventoryItem>
+    {
+        Task<IEnumerable<InventoryItem>> GetAvailableItemsAsync();
+    }
+}
