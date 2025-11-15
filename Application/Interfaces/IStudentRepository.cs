41a4014d8d09c396f6d086e02a8ac8d8a7d416b6
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-11-15 17:34:17 +0300

    feat() : created

---
+++ Application/Interfaces/IStudentRepository.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Entities;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IStudentRepository : IGenericRepository<Student>
+    {
+        Task<Student?> GetByStudentNumberAsync(string studentNumber);
+    }
+}
