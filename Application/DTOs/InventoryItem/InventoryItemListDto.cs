0c8d78a95026f1a90cedf618dfd6168f93bdf1fe
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-11-16 19:07:27 +0300

    feat(dto) : created

---
+++ Application/DTOs/InventoryItem/InventoryItemListDto.cs
@@ -0,0 +1,15 @@
+﻿// InventoryItemListDto.cs
+namespace Application.DTOs.InventoryItem
+{
+    public class InventoryItemListDto
+    {
+        public int Id { get; set; }
+        public string ItemCode { get; set; } = null!;
+        public string Category { get; set; } = null!;
+        public string? Brand { get; set; }
+        public string? Model { get; set; }
+        public string? SerialNumber { get; set; }
+        public string Status { get; set; } = null!;
+        public string? Location { get; set; }
+    }
+}
