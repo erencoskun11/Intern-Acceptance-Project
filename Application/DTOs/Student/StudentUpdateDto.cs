d7c5dc33da1c8c2cd4a1ece6db2d80cb1ec9ecf3
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-11-17 19:29:01 +0300

    feat(dto) : created

---
+++ Application/DTOs/Student/StudentUpdateDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.DTOs.Student
+{
+    public class StudentUpdateDto
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; } = null!;
+
+        public string? StudentNumber { get; set; }
+
+        public int ClassYear { get; set; }
+
+        public int UniversityId { get; set; }
+
+    }
+}
