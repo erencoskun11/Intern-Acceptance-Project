6160d4d02a94a9d77e148fcfba389b92c5c61536
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-11-22 10:18:49 +0300

    created

---
+++ Application/Services/DepartmentService.cs
@@ -0,0 +1,16 @@
+﻿using Application.DTOs.Department;
+using Application.Interfaces;
+using Application.Services.Interfaces;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Services
+{
+    public class DepartmentService : GenericService<Department, DepartmentListDto, DepartmentCreateDto, DepartmentUpdateDto>, IDepartmentService
+    {
+        public DepartmentService(IGenericRepository<Department> repository, IMapper mapper)
+            : base(repository, mapper)
+        {
+        }
+    }
+}
