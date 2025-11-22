c063c4cdd98baeead3f41d7acc029694ebf21b9b
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-11-22 10:17:15 +0300

    feat(entity): created

---
+++ Domain/Entities/Intern.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Common;
+using Domain.Enums;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities
+{
+    public class Intern : BaseEntity
+    {
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+
+        [NotMapped]
+        public string FullName => $"{FirstName} {LastName}";
+
+        public string Email { get; set; } = null!;
+        public string Phone { get; set; } = null!;
+        public string? StudentNumber { get; set; }
+        public int ClassYear { get; set; }
+
+        // --- TÜRKÇE ENUM KULLANIMI ---
+        public DegreeType Degree { get; set; } = DegreeType.Lisans;
+
+        // --- ÇALIŞMA GÜNÜ ---
+        [Range(1, 7)]
+        public int WorkDaysPerWeek { get; set; } = 5;
+
+        public int UniversityId { get; set; }
+        public University? University { get; set; }
+
+        public ICollection<InternshipApplication> Applications { get; set; } = new List<InternshipApplication>();
+        public ICollection<Assignment> Assignments { get; set; } = new List<Assignment>();
+    }
+}
