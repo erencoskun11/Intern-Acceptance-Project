46513d5c91de4cf3f85bd3e89c9a29b9ced2cf04
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-11-23 21:02:51 +0300

    created

---
+++ Infrastructure/Repositories/InventoryItemRepository.cs
@@ -0,0 +1,16 @@
+﻿// InventoryItemRepository.cs
+using Domain.Entities;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Application.Interfaces;
+
+namespace Infrastructure.Repositories
+{
+    public class InventoryItemRepository : GenericRepository<InventoryItem>, IInventoryItemRepository
+    {
+        public InventoryItemRepository(AppDbContext context) : base(context) { }
+
+        public async Task<IEnumerable<InventoryItem>> GetAvailableItemsAsync() =>
+            await _context.InventoryItems.Where(i => i.Status == Domain.Enums.ItemStatus.Available).ToListAsync();
+    }
+}
