93c9103724bca12814947dc531eabbb569c45d70
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-11-30 15:31:00 +0300

    created

--- Application/DTOs/Employee/EmployeeUpdateDto.cs
+++ Application/DTOs/Employee/EmployeeUpdateDto.cs
@@ -1,15 +1,14 @@
-﻿// EmployeeUpdateDto.cs
-namespace Application.DTOs.Employee
+﻿namespace Application.DTOs.Employee
 {
     public class EmployeeUpdateDto
     {
         public int Id { get; set; }
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
-        public string? Email { get; set; }
-        public string? Phone { get; set; }
-        public string? Role { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Phone { get; set; } = null!;
+        public string Role { get; set; } = null!;
         public int? DepartmentId { get; set; }
-        public DateTime? EndDate { get; set; }
+        public DateTime StartDate { get; set; }
     }
 }
