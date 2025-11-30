ee7c18733ad5340e9e5507269288c1c76a53325e
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-11-30 15:33:45 +0300

    feat(IService) : created

---
+++ Application/Services/Interfaces/IInternService.cs
@@ -0,0 +1,14 @@
+﻿using Application.DTOs.Intern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services.Interfaces
+{
+    public interface IInternService : IGenericService<InternListDto,InternCreateDto,InternUpdateDto>    
+    {
+        Task<IEnumerable<InternListDto>> GetAllWithDetailsAsync();
+    }
+}
