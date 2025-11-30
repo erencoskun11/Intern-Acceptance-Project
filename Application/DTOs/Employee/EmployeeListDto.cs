f1c027eca3adeede0d2f136170f1a184c5709327
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-11-30 15:30:01 +0300

    feat(dto): created

--- Application/DTOs/Employee/EmployeeListDto.cs
+++ Application/DTOs/Employee/EmployeeListDto.cs
@@ -1,15 +1,17 @@
-﻿// EmployeeListDto.cs
-namespace Application.DTOs.Employee
+﻿namespace Application.DTOs.Employee
 {
     public class EmployeeListDto
     {
         public int Id { get; set; }
+
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
         public string FullName { get; set; } = null!;
         public string Email { get; set; } = null!;
         public string Phone { get; set; } = null!;
         public string Role { get; set; } = null!;
+        public DateTime StartDate { get; set; }
         public int? DepartmentId { get; set; }
-
-        public string? DepartmentName {  get; set; }=null!;   
+        public string? DepartmentName { get; set; } = null!;
     }
 }
