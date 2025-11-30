9f184009d1e3166aa530273344b8c1df419b0100
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-11-30 15:32:50 +0300

    feat(dto) : created

---
+++ Application/DTOs/Intern/InternCreateDto.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Enums;
+
+namespace Application.DTOs.Intern
+{
+    public class InternCreateDto
+    {
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Phone { get; set; } = null!;
+        public string? StudentNumber { get; set; }
+        public int ClassYear { get; set; }
+
+        public DegreeType Degree { get; set; }  // Türkçe Enum Tipi
+        public int WorkDaysPerWeek { get; set; }
+        public DateTime StartDate { get; set; } = DateTime.UtcNow;
+        public DateTime EndDate { get; set; } = DateTime.UtcNow.AddMonths(1);
+
+        public int UniversityId { get; set; }
+    }
+}
