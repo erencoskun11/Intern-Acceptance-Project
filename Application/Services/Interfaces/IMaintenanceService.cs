f10b8a5c1c2a2556db6b30e98f996697d23c1bae
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-12-02 23:50:54 +0300

    created

---
+++ Application/Services/Interfaces/IMaintenanceService.cs
@@ -0,0 +1,15 @@
+﻿using Application.Common; // Response için
+using Application.DTOs.Maintenance;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Application.Services.Interfaces
+{
+    public interface IMaintenanceService : IGenericService<MaintenanceListDto, MaintenanceCreateDto, MaintenanceUpdateDto>
+    {
+        // Hata Çözümü: Dönüş tipleri Task<Response<...>> olmalı
+        Task<Response<IEnumerable<MaintenanceListDto>>> GetByInventoryItemIdAsync(int itemId);
+
+        Task<Response<IEnumerable<MaintenanceListDto>>> GetAllWithDetailsAsync();
+    }
+}
