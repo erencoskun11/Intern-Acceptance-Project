8b4226946b784eeb4147a71f54188c79851d0092
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-12-03 19:22:04 +0300

    feat(dto) : created

---
+++ Application/DTOs/University/UniversityUpdateDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.DTOs.University
+{
+    public class UniversityUpdateDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+    }
+}
