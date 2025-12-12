bec879f96448e163a21788b2f5671c8bb81689a2
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-12-12 19:35:27 +0300

    created

---
+++ Application/Services/Interfaces/IEmployeeService.cs
@@ -0,0 +1,13 @@
+﻿using Application.DTOs.Employee;
+using Domain.Entities;
+
+namespace Application.Services.Interfaces
+{
+    public interface IEmployeeService : IGenericService<EmployeeListDto, EmployeeCreateDto, EmployeeUpdateDto>
+    {
+        // Opsiyonel: özel metotlar eklenebilir
+        Task<EmployeeListDto?> GetByEmailAsync(string email);
+
+        Task<IEnumerable<EmployeeListDto>> GetByDepartmentIdAsync(int departmentId);
+    }
+}
