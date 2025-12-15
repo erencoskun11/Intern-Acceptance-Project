6fc7296dfa69fac87e9c5d9133ab1a59c8421842
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-12-15 21:52:45 +0300

    feat(exception) : created

---
+++ Application/Exceptions/ItemNotAvailableException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Application.Exceptions
+{
+    public class ItemNotAvailableException : Exception
+    {
+        public ItemNotAvailableException() { }
+        public ItemNotAvailableException(string message) : base(message) { }
+    }
+}
