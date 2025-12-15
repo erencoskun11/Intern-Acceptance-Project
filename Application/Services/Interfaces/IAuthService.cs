cb3817d5198c38e64fc46228f07256fce5be8f77
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-12-15 21:56:12 +0300

    feat(IService): created

---
+++ Application/Services/Interfaces/IAuthService.cs
@@ -0,0 +1,17 @@
+﻿
+using Application.Common;
+using Application.DTOs.Auth;
+using Domain.Entities;
+using System.Threading.Tasks;
+
+namespace Application.Services.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<Response<string>> LoginAsync(LoginDto loginDto);
+        Task<Response<NoContent>> DeleteUserAsync(int id);
+        Task<Response<AppUser>> GetMeAsync(string userName);
+
+        Task<Response<AppUser>> CreateUserAsync(RegisterDto registerDto);
+    }
+}
