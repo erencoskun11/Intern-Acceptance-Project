f8c81249a4adaa004e8d8bb68e5ae3435fe58f8b
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-12-17 18:32:42 +0300

    feat(mappingTest) : created

---
+++ Stajyer_Projesi.Tests/Systems/Mappings/EmployeeMappingTests.cs
@@ -0,0 +1,57 @@
+﻿using Application.DTOs.Employee;
+using Application.Mappings;
+using AutoMapper;
+using Domain.Entities;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stajyer_Projesi.Tests.Systems.Mappings
+{
+    public class EmployeeMappingTests
+    {
+        private readonly IMapper _mapper;
+        public EmployeeMappingTests()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MappingProfile>();
+            });
+            _mapper = config.CreateMapper();
+        }
+
+        [Fact]
+        public void Configuration_Should_Be_Valid()
+        {
+            var config = _mapper.ConfigurationProvider;
+            config.AssertConfigurationIsValid();
+        }
+
+        [Fact]
+        public void Should_Map_Employee_To_EmployeeListDto_Correctly()
+        {
+            var employee = new Employee { Id = 1, FirstName = "Test", LastName="User"};
+
+            var dto = _mapper.Map<EmployeeListDto>(employee);
+
+            dto.Should().NotBeNull();
+            dto.FirstName.Should().Be("Test");
+            dto.LastName.Should().Be("User");
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
