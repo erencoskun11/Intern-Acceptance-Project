b46939427bbb3f9f7d1f07a7e05d16ebb8ded610
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-12-18 22:18:14 +0300

    feat(SignalR) : created

---
+++ Stajyer_Projesi/Services/SignalRService.cs
@@ -0,0 +1,31 @@
+﻿using Application.Interfaces;
+using Application.Services.Interfaces;
+using Microsoft.AspNetCore.SignalR;
+using Stajyer_Projesi.Hubs;
+
+namespace Stajyer_Projesi.Services
+{
+    public class SignalRService : ISignalRService
+    {
+        private readonly IHubContext<InventoryHub, IInventoryHub> _hubContext;
+
+        public SignalRService(IHubContext<InventoryHub,IInventoryHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+        public async Task RefreshDashboardAsync()
+        {
+            await _hubContext.Clients.All.ReceiveDashboardUpdate();
+        }
+
+        public async Task RefreshEntityListAsync(string entityName)
+        {
+            await _hubContext.Clients.All.ReceiveEntityUpdate(entityName);
+        }
+
+        public async Task SendNotificationAsync(string message, string type = "success")
+        {
+            await _hubContext.Clients.All.ReceiveNotification(message,type);
+        }
+    }
+}
