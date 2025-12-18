ceafedd47e860cb97862086650836e229b60543f
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-12-18 22:20:20 +0300

    feat(validation) : created

---
+++ Stajyer_Projesi.Tests/Systems/Validations/AssignmentValidationTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stajyer_Projesi.Tests.Systems.Validations
+{
+    internal class AssignmentValidationTests
+    {
+    }
+}
