868e1c01eb0be0da805a44dfa34025ddb11cb3ba
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-12-20 21:41:34 +0300

    feat(interface) : created

---
+++ Application/Services/Interfaces/ICacheService.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.Services.Interfaces
+{
+    public interface ICacheService
+    {
+        bool TryGet<T>(string key, out T value);
+
+        void Set<T>(string key, T value, TimeSpan? expiration = null);
+
+        void Remove(string key);
+    }
+}
