423ca101f8cf9f56ea1f437ad6106b54b1e7c866
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-12-21 10:43:45 +0300

    feat(interface) : SignalR

---
+++ Application/Services/Interfaces/ISignalRService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services.Interfaces
+{
+    public interface ISignalRService
+    {
+        Task SendNotificationAsync(string message,string type = "success");
+        Task RefreshEntityListAsync(string entityName); 
+        Task RefreshDashboardAsync();
+    }
+}
