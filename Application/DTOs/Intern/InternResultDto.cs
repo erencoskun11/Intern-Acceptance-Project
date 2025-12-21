74d93c8b9faa4a95b82f0d4ac4dd38018551b713
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-12-21 10:41:40 +0300

    feat(dto) : internResult

---
+++ Application/DTOs/Intern/InternResultDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.Intern
+{
+    public class InternResultDto
+    {
+        // SQL fonksiyonundaki "NewId" kolonunu karşılar
+        public int NewId { get; set; }
+
+        // SQL fonksiyonundaki "ErrorMessage" kolonunu karşılar
+        public string? ErrorMessage { get; set; }
+    }
+}
