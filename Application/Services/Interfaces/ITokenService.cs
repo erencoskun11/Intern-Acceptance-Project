5a6efb40a0b811050dad772e231821d8359348f9
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-12-22 21:09:15 +0300

    feat(interface) : created

---
+++ Application/Services/Interfaces/ITokenService.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+using System.Collections.Generic;
+
+namespace Application.Services.Interfaces
+{
+    public interface ITokenService
+    {
+        string CreateToken(Employee employee, IList<string> roles);
+
+        string CreateToken(Intern student, IList<string> roles);
+    }
+}
