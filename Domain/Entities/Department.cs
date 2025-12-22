4dea4cb36f5bb86631de752e1952fe68a8ccaee5
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-12-22 21:11:00 +0300

    updated

--- Domain/Entities/Department.cs
+++ Domain/Entities/Department.cs
@@ -1,22 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Domain.Common;
+
 
 namespace Domain.Entities
 {
-    public class Department
-    {
-        public int Id { get; set; }
-
-        [Required]
-        [MaxLength(100)]
+    public class Department :BaseEntity
+    {     
         public string Name { get; set; } = null!;
 
-        // Iliskiler
-        public ICollection<Mentor>? Mentors { get; set; }
-        public ICollection<InternshipApplication>? Applications { get; set; }
+        
+        public ICollection<Employee>? Employees { get; set; }
     }
 }
