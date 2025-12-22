fd6f621aded0f504e6044b83f87b8f51656a39e1
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-12-22 21:06:22 +0300

    feat(interface) : created

---
+++ Application/Services/Interfaces/IGenericService.cs
@@ -0,0 +1,18 @@
+﻿using Application.Common; // Response burada
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Application.Services.Interfaces
+{
+    public interface IGenericService<TListDto, TCreateDto, TUpdateDto>
+    {
+        // Dönüş tiplerini Response<T> yaptık
+        Task<Response<int>> CreateAsync(TCreateDto dto);
+        Task<Response<NoContent>> UpdateAsync(int id, TUpdateDto dto);
+        Task<Response<NoContent>> DeleteAsync(int id);
+        Task<Response<IEnumerable<TListDto>>> GetAllAsync();
+        Task<Response<TListDto>> GetByIdAsync(int id);
+
+    }
+}
