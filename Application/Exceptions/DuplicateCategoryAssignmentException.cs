88b84f8254dd3ea1ca7d7998fe0ebdfbb32e563d
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-12-22 21:02:34 +0300

    feat(exception) : created

---
+++ Application/Exceptions/DuplicateCategoryAssignmentException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Application.Exceptions
+{
+    public class DuplicateCategoryAssignmentException : Exception
+    {
+        public DuplicateCategoryAssignmentException() { }
+        public DuplicateCategoryAssignmentException(string message) : base(message) { }
+    }
+}
