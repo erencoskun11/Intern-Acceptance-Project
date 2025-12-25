d57cfcf02a77a5dc8e752ef0a210b903ee407f5c
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-12-25 22:46:56 +0300

    feat(interface) : created

---
+++ Application/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entities;
+using System;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IEmployeeRepository Employees { get; }
+        IInternRepository Interns { get; }
+        IDepartmentRepository Departments { get; }
+        IUniversityRepository Universities { get; }
+        IInventoryItemRepository InventoryItems { get; }
+        IAssignmentRepository Assignments { get; }
+        IMaintenanceRepository Maintenances { get; }
+
+        IGenericRepository<AppUser> AppUsers { get; }
+
+        Task<int> CommitAsync();
+    }
+}
