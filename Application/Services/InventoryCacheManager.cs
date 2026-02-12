e12d3fffa57a2bd588db698a8719f23f4bc2de6d
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2026-02-12 17:23:39 +0300

    feat(manager): created

---
+++ Application/Services/InventoryCacheManager.cs
@@ -0,0 +1,20 @@
+﻿using Application.Constants;
+using Application.Services.Interfaces;
+
+namespace Application.Services
+{
+    public class InventoryCacheManager : IInventoryCacheManager
+    {
+        private readonly ICacheService _cacheService;
+
+        public InventoryCacheManager(ICacheService cacheService)
+        {
+            _cacheService = cacheService;
+        }
+        public void ClearAll()
+        {
+            _cacheService.Remove(CacheKeys.InventoryItemList);
+            _cacheService.Remove(CacheKeys.InventoryAvailableList);
+        }
+    }
+}
