00afb79ca357bf9901839c47791587a8145e3424
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2026-02-17 17:52:22 +0300

    feat(application-university): add UniversityService with cache integration and bulk operations

--- Application/Services/UniversityService.cs
+++ Application/Services/UniversityService.cs
@@ -1,21 +1,59 @@
 ﻿using Application.Common;
+using Application.Constants;
 using Application.DTOs.University;
 using Application.Interfaces;
 using Application.Services.Interfaces;
 using AutoMapper;
 using Domain.Entities;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace Application.Services
 {
     public class UniversityService : GenericService<University, UniversityListDto, UniversityCreateDto, UniversityUpdateDto>, IUniversityService
     {
-        // CS7036 ÇÖZÜMÜ
-        public UniversityService(IGenericRepository<University> repository, IMapper mapper, IUnitOfWork uow)
+        ICacheService _cacheService;
+        public UniversityService(IGenericRepository<University> repository, IMapper mapper, IUnitOfWork uow,ICacheService cacheService)
             : base(repository, mapper, uow)
         {
+            _cacheService = cacheService;
+        }
+
+        public override async Task<Response<IEnumerable<UniversityListDto>>> GetAllAsync()
+        {
+            if(_cacheService.TryGet(CacheKeys.UniversityList, out IEnumerable<UniversityListDto> cachedList))
+            {
+                return Response<IEnumerable<UniversityListDto>>.Success(cachedList, 200);
+            }
+
+            var response = await base.GetAllAsync();
+
+            if (response.IsSuccessful)
+            {
+                _cacheService.Set(CacheKeys.UniversityList, response.Data, TimeSpan.FromMinutes(30));
+            }
+            return response;
+        }
+        public override async Task<Response<int>> CreateAsync(UniversityCreateDto dto)
+        {
+           var response = await base.CreateAsync(dto);
+            if (response.IsSuccessful)
+            {
+                _cacheService.Remove(CacheKeys.UniversityList);
+            }
+            return response;
+        }
+
+        public override async Task<Response<NoContent>> UpdateAsync(int id ,UniversityUpdateDto dto)
+        {
+            var response = await base.UpdateAsync(id, dto);
+            if (response.IsSuccessful)_cacheService.Remove(CacheKeys.UniversityList);
+            return response;
+        }
+        public override async Task<Response<NoContent>> DeleteAsync(int id)
+        {
+            var response = await base.DeleteAsync(id);
+            if (response.IsSuccessful) _cacheService.Remove(CacheKeys.UniversityList);
+            return response;
         }
 
         public async Task<Response<NoContent>> BulkCreateAsync(IEnumerable<UniversityCreateDto> dtos)
@@ -30,7 +68,6 @@ namespace Application.Services
                 await _repository.AddAsync(entity);
             }
 
-            // GenericRepository'de SaveChanges sildiğimiz için UoW ile kaydediyoruz
             await _uow.CommitAsync();
 
             return Response<NoContent>.Success(201);
