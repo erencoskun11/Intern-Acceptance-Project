a4a4aff78d9dab1c3fdfd6c86c05846a68de6a48
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2026-02-18 17:52:34 +0300

    migration

---
+++ Infrastructure/Migrations/20251201171611_RenameStudentTableAndAddUserRelation.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameStudentTableAndAddUserRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Assignments_Students_InternId",
+                table: "Assignments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Students_Universities_UniversityId",
+                table: "Students");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Students",
+                table: "Students");
+
+            migrationBuilder.RenameTable(
+                name: "Students",
+                newName: "Interns");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Students_UniversityId",
+                table: "Interns",
+                newName: "IX_Interns_UniversityId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Interns",
+                table: "Interns",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Assignments_Interns_InternId",
+                table: "Assignments",
+                column: "InternId",
+                principalTable: "Interns",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Interns_Universities_UniversityId",
+                table: "Interns",
+                column: "UniversityId",
+                principalTable: "Universities",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Assignments_Interns_InternId",
+                table: "Assignments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Interns_Universities_UniversityId",
+                table: "Interns");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Interns",
+                table: "Interns");
+
+            migrationBuilder.RenameTable(
+                name: "Interns",
+                newName: "Students");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Interns_UniversityId",
+                table: "Students",
+                newName: "IX_Students_UniversityId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Students",
+                table: "Students",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Assignments_Students_InternId",
+                table: "Assignments",
+                column: "InternId",
+                principalTable: "Students",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Students_Universities_UniversityId",
+                table: "Students",
+                column: "UniversityId",
+                principalTable: "Universities",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
