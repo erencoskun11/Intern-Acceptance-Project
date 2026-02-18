81a57d8cef12cfa1ebbe166beda2d7be1c1692a6
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2026-02-18 17:46:03 +0300

    feat(assignment-config): add EF Core configuration with constraints and relations

---
+++ Infrastructure/Configurations/AssignmentConfiguration.cs
@@ -0,0 +1,42 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configurations
+{
+    public class AssignmentConfiguration : IEntityTypeConfiguration<Assignment>
+    {
+        public void Configure(EntityTypeBuilder<Assignment> builder)
+        {
+            builder.ToTable("Assignments", t =>
+            {
+                t.HasCheckConstraint("CK_Assignments_Assignee",
+                    "(\"InternId\" IS NOT NULL AND \"EmployeeId\" IS NULL) OR (\"InternId\" IS NULL AND \"EmployeeId\" IS NOT NULL)");
+            });
+
+            builder.HasKey(a => a.Id);
+
+            builder.Property(a => a.AssignedAt)
+                    .HasDefaultValueSql("now() at time zone 'utc'");
+
+
+            builder.Property(a => a.ActualReturnAt)
+                    .HasColumnType("timestamptz");
+
+            builder.HasOne(a => a.InventoryItem)
+                    .WithMany(i => i.Assignments)
+                    .HasForeignKey(a => a.InventoryItemId)
+                    .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(a => a.Intern)
+                    .WithMany(s => s.Assignments)
+                    .HasForeignKey(a => a.InternId)
+                    .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(a => a.Employee)
+                    .WithMany(e => e.Assignments)
+                    .HasForeignKey(a => a.EmployeeId)
+                    .OnDelete(DeleteBehavior.SetNull);
+        }
+    }
+}
