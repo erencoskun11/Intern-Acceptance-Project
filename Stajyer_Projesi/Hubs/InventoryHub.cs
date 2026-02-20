780e14e5749df30d550e706259cfc5e27af7aefa
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2026-02-20 17:34:46 +0300

    feat(hubs): create InventoryHub with [Authorize] attribute

---
+++ Stajyer_Projesi/Hubs/InventoryHub.cs
@@ -0,0 +1,15 @@
+﻿using Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Stajyer_Projesi.Hubs
+{
+    [Authorize]
+    public class InventoryHub : Hub<IInventoryHub>
+    {
+        public override async Task OnConnectedAsync()
+        {
+            await base.OnConnectedAsync();
+        }
+    }
+}
